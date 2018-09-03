a4d8d1bc7182d6270d3b8632d5aace6cc0d970ad
Ioan GHERMAN <ioan.gherman95@yahoo.com>
2018-09-03 22:16:56 +0300

    modifications + webcam support

--- faceRecognition.py
+++ faceRecognition.py
@@ -17,6 +17,8 @@ class FaceRecognizer():
         with CustomObjectScope({'tf': tf}):
             nn_model = load_model(MODEL_PATH)
         self.nn_model = nn_model
+        self.classifier = None
+        self.encoder = None
 
     # returns all embeddings
     def obtainImageEmbedding(self, face):
@@ -25,11 +27,119 @@ class FaceRecognizer():
     
         return embedding
 
+    # used to load the svm classifier
+    def loadSVMClassifier(self):
+        # load known embeddings
+        print("Loading svm classifier...\nLoading known embeddings.")
+        data = loadTrainingImagesEmbeddings()
+
+        # have fun
+        targets = ([k for k in data])
+
+        print(targets)
+
+        encoder = LabelEncoder()
+        encoder.fit(targets)
+        y = encoder.transform(targets)
+
+        X_train = []
+        X_test = []
+        Y_train = []
+        Y_test = []
+
+        # split the data 70% training 30% testing
+        for label in data:
+
+            # randomly pick 30%
+            no = int(0.3 * len(data[label]))
+            indxs = list(range(0,len(data[label])))
+
+            # obtain random indexes
+            rand_indexes = random.sample(indxs, no)
+
+            for i, embd in enumerate(data[label]):
+                if i in rand_indexes:
+                    X_test.append(embd)
+                    Y_test.extend(encoder.transform([label]))
+                else:
+                    X_train.append(embd)
+                    Y_train.extend(encoder.transform([label]))
+
+        svc = SVC() # default kernel is rbf 
+        svc.fit(X_train, Y_train)
+
+        accuracy = accuracy_score(Y_test, svc.predict(X_test))
+        print("Accuracy = {}".format(accuracy))
+
+        self.classifier = svc
+        self.encoder = encoder
+
+    # used to load the knn classifier
+    def loadKNNClassifier(self):
+        # load known embeddings
+        print("Loading knn classifier...\nLoading known embeddings.")
+        data = loadTrainingImagesEmbeddings()
+
+        # have fun
+        targets = ([k for k in data])
+
+        print(targets)
+
+        encoder = LabelEncoder()
+        encoder.fit(targets)
+        y = encoder.transform(targets)
+
+        X_train = []
+        X_test = []
+        Y_train = []
+        Y_test = []
+
+        # split the data 70% training 30% testing
+        for label in data:
+
+            # randomly pick 30%
+            no = int(0.3 * len(data[label]))
+            indxs = list(range(0,len(data[label])))
+
+            # obtain random indexes
+            rand_indexes = random.sample(indxs, no)
+
+            for i, embd in enumerate(data[label]):
+                if i in rand_indexes:
+                    X_test.append(embd)
+                    Y_test.extend(encoder.transform([label]))
+                else:
+                    X_train.append(embd)
+                    Y_train.extend(encoder.transform([label]))
+
+        knn = KNeighborsClassifier(n_neighbors=1, metric="euclidean")
+        knn.fit(X_train, Y_train)
+
+        accuracy = accuracy_score(Y_test, knn.predict(X_test))
+        print("Accuracy = {}".format(accuracy))
+
+        self.classifier = knn
+        self.encoder = encoder
+
+    # classify one embedding
+    def classify(self, emb):
+        if self.classifier is None:
+            print("No classifier is loaded!")
+            return 
+
+        prediction = self.classifier.predict([emb])
+        identity = self.encoder.inverse_transform(prediction)[0]
+
+        #TODO - check for "unknown" person
+        # very big distance or something?
+
+        return identity
+
+
 
 # used to detect the faces (and their landmarks) from a photo and align them
 # using dlib and OpenFace lib
-def detectAndAlignFaces(imgPath, align, testing=False):
-    image = cv2.imread(imgPath)
+def detectAndAlignFaces(image, align, testing=False):
     alignedFaces = []
 
     # obtain faces bounding boxes
@@ -50,7 +160,6 @@ def detectAndAlignFaces(imgPath, align, testing=False):
             cv2.waitKey(0)
 
     return alignedFaces, boundingBoxes
-
 
 # go through the training folder and compute the embeddings and save as json
 def saveTrainingImagesEmbeddings(fr, align):
@@ -96,129 +205,27 @@ def loadTrainingImagesEmbeddings():
 
     return data
 
-# classify using KNN
-def classifyKNN(fr, align, imgPath):
-    print("Detecting and aligning face.")
-    faces, _ = detectAndAlignFaces(imgPath, align, testing=False)
-    if faces is None or len(faces) != 1:
-        print("No face (or more than oen face) detected in the picture.")
-        return
+# returns the identities of the recognized faces and their bounding boxes
+def recognizeFacesInImage(img, fr, align):
+    print("Detecting and aligning face(s).")
+    faces, boxes = detectAndAlignFaces(img, align, testing=False)
+    if faces is None:
+        print("No face detected in the picture.")
+        return [], []
 
     # obtain embedding
-    print("Obtaining embedding.")
-    emb = fr.obtainImageEmbedding(faces[0])
+    identities = []
+    print("Obtaining embedding(s) and classify the face(s).")
+    for face in faces:
+        emb = fr.obtainImageEmbedding(face)
+        identities.append(fr.classify(emb))
 
-    # load known embeddings
-    print("Loading known embeddings.")
-    data = loadTrainingImagesEmbeddings()
+    if len(identities) != len(faces) or len(identities) == 0 or len(faces) == 0:
+        print("No faces were recognized in this frame.")
+        return [], []
 
-    # have fun
-    targets = ([k for k in data])
+    return identities, boxes
 
-    print(targets)
-
-    encoder = LabelEncoder()
-    encoder.fit(targets)
-    y = encoder.transform(targets)
-
-    X_train = []
-    X_test = []
-    Y_train = []
-    Y_test = []
-
-    # split the data 70% training 30% testing
-    for label in data:
-
-        # randomly pick 30%
-        no = int(0.3 * len(data[label]))
-        indxs = list(range(0,len(data[label])))
-
-        # obtain random indexes
-        rand_indexes = random.sample(indxs, no)
-
-        for i, embd in enumerate(data[label]):
-            if i in rand_indexes:
-                X_test.append(embd)
-                Y_test.extend(encoder.transform([label]))
-            else:
-                X_train.append(embd)
-                Y_train.extend(encoder.transform([label]))
-
-    knn = KNeighborsClassifier(n_neighbors=3, metric='euclidean')
-    knn.fit(X_train, Y_train)
-
-    accuracy = accuracy_score(Y_test, knn.predict(X_test))
-
-    print("Accuracy = {}".format(accuracy))
-
-    prediction = knn.predict([emb])
-    identity = encoder.inverse_transform(prediction)[0]
-
-    image = cv2.imread(imgPath)
-    cv2.imshow("{}".format(identity), image)
-    cv2.waitKey(0)
-
-# classify using SVM
-def classifySVM(fr, align, imgPath):
-    print("Detecting and aligning face.")
-    faces, _ = detectAndAlignFaces(imgPath, align, testing=False)
-    if faces is None or len(faces) != 1:
-        print("No face (or more than oen face) detected in the picture.")
-        return
-
-    # obtain embedding
-    print("Obtaining embedding.")
-    emb = fr.obtainImageEmbedding(faces[0])
-
-    # load known embeddings
-    print("Loading known embeddings.")
-    data = loadTrainingImagesEmbeddings()
-
-    # have fun
-    targets = ([k for k in data])
-
-    print(targets)
-
-    encoder = LabelEncoder()
-    encoder.fit(targets)
-    y = encoder.transform(targets)
-
-    X_train = []
-    X_test = []
-    Y_train = []
-    Y_test = []
-
-    # split the data 70% training 30% testing
-    for label in data:
-
-        # randomly pick 30%
-        no = int(0.3 * len(data[label]))
-        indxs = list(range(0,len(data[label])))
-
-        # obtain random indexes
-        rand_indexes = random.sample(indxs, no)
-
-        for i, embd in enumerate(data[label]):
-            if i in rand_indexes:
-                X_test.append(embd)
-                Y_test.extend(encoder.transform([label]))
-            else:
-                X_train.append(embd)
-                Y_train.extend(encoder.transform([label]))
-
-    svc = SVC() # default kernel is rbf 
-    svc.fit(X_train, Y_train)
-
-    accuracy = accuracy_score(Y_test, svc.predict(X_test))
-
-    print("Accuracy = {}".format(accuracy))
-
-    prediction = svc.predict([emb])
-    identity = encoder.inverse_transform(prediction)[0]
-
-    image = cv2.imread(imgPath)
-    cv2.imshow("{}".format(identity), image)
-    cv2.waitKey(0)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -228,6 +235,8 @@ def main():
 
     parser.add_argument("--classify-knn", type=str, help="used to classify a face using knn")
     parser.add_argument("--classify-svm", type=str, help="used to classify a face using svm")
+
+    parser.add_argument("--webcam", action="store_true", help="recognize faces from the webcam(if the webcam is available)")
 
     args = parser.parse_args()
     
@@ -261,10 +270,81 @@ def main():
             return
 
     elif args.classify_knn:
-        classifyKNN(fr, align, args.classify_knn)
+        fr.loadKNNClassifier()
+        image = cv2.imread(args.classify_svm)
+        identities, boxes = recognizeFacesInImage(image, fr, align)
+
+        # draw boxes over the recognized faces and label them
+        # inspired from https://www.pyimagesearch.com/2017/04/03/facial-landmarks-dlib-opencv-python/
+        for indx, label in enumerate(identities):
+            left, top, right, bottom = boxes[indx].left(), boxes[indx].top(), boxes[indx].right(), boxes[indx].bottom()
+            cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 1)
+            cv2.putText(image, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
+
+        # show the output image
+        cv2.imshow("Faces", image)
+        cv2.waitKey(0)
 
     elif args.classify_svm:
-        classifySVM(fr, align, args.classify_svm)
+        fr.loadSVMClassifier()
+        image = cv2.imread(args.classify_svm)
+        identities, boxes = recognizeFacesInImage(image, fr, align)
+
+        # draw boxes over the recognized faces and label them
+        for indx, label in enumerate(identities):
+            left, top, right, bottom = boxes[indx].left(), boxes[indx].top(), boxes[indx].right(), boxes[indx].bottom()
+            cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 1)
+            cv2.putText(image, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
+
+        # show the output image
+        cv2.imshow("Faces", image)
+        cv2.waitKey(0)
+
+    elif args.webcam:
+        # video capture code from https://github.com/ageitgey/face_recognition/blob/master/examples/facerec_from_webcam_faster.py
+        video_capture = cv2.VideoCapture(0)
+
+        if video_capture is None or not video_capture.isOpened():
+            print("Error opening webcam.")
+            return
+
+        fr.loadSVMClassifier()
+
+        frame_no = 1
+        while True:
+            if frame_no != 4:
+                frame_no += 1
+                continue
+            else:
+                frame_no = 1
+
+            # Grab a single frame of video and resize
+            ret, frame = video_capture.read()
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            # process frame
+            identities, boxes = recognizeFacesInImage(small_frame, fr, align)
+
+            # when lens are == 0 skip the classification
+            if len(identities) > 0 or len(boxes) > 0:
+                # draw boxes over the recognized faces and label them
+                for indx, label in enumerate(identities):
+                    left, top, right, bottom = boxes[indx].left() * 4, boxes[indx].top() * 4, boxes[indx].right() * 4, boxes[indx].bottom() * 4
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, label, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            # Display the resulting image
+            cv2.imshow('Video', frame)
+
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Release handle to the webcam
+        video_capture.release()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
